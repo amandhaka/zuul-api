548271a358a3082d150325b07a79cb3ad713523b
amandhaka <amandhaka2021@gmail.com>
2021-03-06 14:26:10 +0530

    ZUul

--- src/main/java/com/example/zuulapi/ZuulApiApplication.java
+++ src/main/java/com/example/zuulapi/ZuulApiApplication.java
@@ -2,11 +2,12 @@ package com.example.zuulapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableZuulProxy
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ZuulApiApplication {
 
 	public static void main(String[] args) {
